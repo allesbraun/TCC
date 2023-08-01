3acbce9e8e42fc80722b802b1e0e349299fcccc7
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-01 01:06:21 -0300

    comentários atualizados

--- data/data_analyzer/data_response.py
+++ data/data_analyzer/data_response.py
@@ -18,12 +18,12 @@ def data_response(content, file):
         # 'num_priority': count_priority(content), # Number of priority queues instantiated
         # 'num_binSearch': count_binSearch(content), # Number of calls for a binary search
         # 'num_minMax': count_minMax(content), # Number data of calls for min() or max() functions
-        'num_sort': count_sorts(content), # Number of calls for the sort() function
+        'num_sort': count_sorts(content), # Number of calls for the sort() function #OK
         # 'num_hash_map': count_hash_maps(content), # Number of hash maps instantiated
         # 'num_hash_set': count_hash_sets(content), # Number of hash sets instantiated
         # 'num_recursive': count_recursive(content), # Number of recursive calls for a given function
         # 'num_nested_loop': count_nested_loops(content), # Depth of nested loops
-        'num_vari': count_variables(content), # Number of variables declared
+        'num_vari': count_variables(content), # Number of variables declared #NÃO CONTO ARRAYS MAS AINDA NÃO BATE SEMPRE COM OS CRAWLED CODES
         # 'num_method': count_methods(content), # Number of methods declared
         # 'num_state': count_statements(content), # Number of statements
     }
