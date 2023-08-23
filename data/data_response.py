036d3d43d12243d7c6a5a8af63f38af9c437b110
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-23 10:14:55 -0300

    binSearch ainda conta recursões

--- data/data_response.py
+++ data/data_response.py
@@ -7,21 +7,21 @@ from data_classificators.autogluon_classificator import autogluon_classifier
 def data_response(content, file):
     response_data = {
         'num_if': count_ifs(content), # Number of if statements in the code #OK
-        'num_else': count_elses(content), # Number of else statements in the code #NÃO DEVIA SER CONTADO NO ELSE IF 
+        'num_else': count_elses(content), # Number of else statements in the code #OK?
         'num_switch': count_switches(content), #Number of switch statements #OK
         'num_loop': count_loops(content), # Number of loops, including for and while statements #OK
         'num_break': count_breaks(content), # Number of break statements #OK
         'num_priority': count_priority(content), # Number of priority queues instantiated #OK
-        'num_binSearch': count_binSearch(content), # Number of calls for a binary search #NÃO CONSIGO EVITAR QUE ELE CONTE REPETIDO QUANDO TEM RECURSÃO
+        'num_binSearch': count_binSearch(content), # Number of calls for a binary search #
         'num_minMax': count_minMax(content), # Number data of calls for min() or max() functions #OK
         'num_sort': count_sorts(content), # Number of calls for the sort() function #OK
         'num_hash_map': count_hash_maps(content), # Number of hash maps instantiated #OK
         'num_hash_set': count_hash_sets(content), # Number of hash sets instantiated #OK
-        'num_recursive': count_recursive(content), # Number of recursive calls for a given function #TA ERRADO
-        'num_nested_loop': count_nested_loops(content), # Depth of nested loops no código #Maximumsumrectangleina2DmatrixDP27GeeksforGeeks-1.java minha versão conta 5 e eu contei 5 mas a do professor conta 3
-        'num_vari': count_variables(content), # Number of variables declared #NÃO CONTO ARRAYS MAS AINDA NÃO BATE SEMPRE COM OS CRAWLED CODES
+        'num_recursive': count_recursive(content), # Number of recursive calls for a given function #OK
+        'num_nested_loop': count_nested_loops(content), # Depth of nested loops no código #OK
+        'num_vari': count_variables(content), # Number of variables declared #
         'num_method': count_methods(content), # Number of methods declared #OK
-        'num_state': count_statements(content), # Number of statements #O QUE EU DEVO CONSIDERAR STATEMENTS?
+        'num_state': count_statements(content), # Number of statements #OK
         'filename': file.name, #OK
     }
     
--- data_analyzer/binary_search_counter.py
+++ data_analyzer/binary_search_counter.py
@@ -1,25 +1,10 @@
 import javalang
 
 
-def count_binSearch(java_code):
-    # parser = javalang.parser.Parser()
-    # tree = parser.parse(java_code)
-    
-    # def count_recursive_calls(node, target_name, visited_methods):
+def count_binSearch(content):
+    tree = javalang.parse.parse(content)
     count = 0
-        
-        # if isinstance(node, javalang.tree.MethodInvocation) and node.member == target_name:
-        #     if target_name not in visited_methods:
-        #         visited_methods.add(target_name)
-        #         count += 1
-        
-        # for child_node in node.children:
-        #     count += count_recursive_calls(child_node, target_name, visited_methods)
-        
+    for _, node in tree.filter(javalang.tree.MethodInvocation):
+        if node.member == 'binarySearch':
+            count += 1
     return count
-    
-    # target_name = "binarySearch"  # Nome do método a ser contado
-    # visited_methods = set()       # Conjunto para rastrear métodos visitados
-    
-    # total_binary_search_calls = count_recursive_calls(tree, target_name, visited_methods)
-    # return total_binary_search_calls
