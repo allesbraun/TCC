fad6058164642d606ed25cb140d0b1ce6813ef44
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-24 12:53:31 -0300

    num_vari fixed

--- data_analyzer/variable_counter.py
+++ data_analyzer/variable_counter.py
@@ -6,7 +6,7 @@ def count_variables(content):
     tree = javalang.parse.parse(content)
     count = 0
 
-    count += sum(1 for _ in tree.filter(javalang.tree.LocalVariableDeclaration))
+    # count += sum(1 for _ in tree.filter(javalang.tree.LocalVariableDeclaration))
     count += sum(1 for _ in tree.filter(javalang.tree.VariableDeclaration))
 
     return count 
