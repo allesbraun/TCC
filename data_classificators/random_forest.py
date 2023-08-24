b1dd341d5cbde9dacc883b6c7ee1431c813d6cec
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-24 14:38:22 -0300

    autogluon as the only classifier

--- data_classificators/random_forest.py
+++
@@ -1,6 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-
-#creating an instance of the RandomForestClassifier
-rfc = RandomForestClassifier(random_state=42)
