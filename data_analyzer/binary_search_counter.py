93b7e4f9f816e638b0191d862df47f620ccbc517
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-24 14:37:59 -0300

    binSearch fixed

--- data_analyzer/binary_search_counter.py
+++ data_analyzer/binary_search_counter.py
@@ -3,8 +3,13 @@ import javalang
 
 def count_binSearch(content):
     tree = javalang.parse.parse(content)
+    currently_analyzed_methods = set()
     count = 0
     for _, node in tree.filter(javalang.tree.MethodInvocation):
         if node.member == 'binarySearch':
-            count += 1
+            if node.member in currently_analyzed_methods:
+                continue  # Skip this occurrence, it's a recursive call
+            else:
+                currently_analyzed_methods.add(node.member)
+                count += 1
     return count
