5092e37799e001fe3b18422925964c83a9dd0dc2
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-08-31 04:37:56 -0300

    generating first complete csv files

--- data_classificators/autogluon_classificator.py
+++ data_classificators/autogluon_classificator.py
@@ -3,43 +3,38 @@ import os
 import pandas as pd
 from autogluon.tabular import TabularPredictor
 
+import AutogluonModels
 from databases import *
 
 
 def autogluon_classifier(code_csv):
         
     test_data = code_csv
-    predictor = TabularPredictor.load('first_training')
-    predictor.leaderboard(test_data, silent=True)
+    predictor_efficiency = TabularPredictor.load("AutogluonModels/ag-efficiency_training", require_py_version_match=False)
+    # predictor_efficiency.leaderboard(test_data, silent=True)
     # Make predictions
-    predictions = predictor.predict(test_data)
-    return predictions
+    predictions_efficiency = predictor_efficiency.predict(test_data)
+    
+    efficiency = pd.Series([predictions_efficiency[0]], name='efficiency')
 
-def autogluon_trainer():
-    #x_class, y_class, x_efficiency, y_efficiency = smote()
-    # Caminho para a pasta que contém o arquivo datasets
-    path_datasets = os.path.dirname(os.path.abspath(__file__))
-    path_databases = os.path.join(path_datasets, '..', 'databases')
-    path_database = os.path.join(path_databases, 'merged_database.csv')
-    # Load and prepare your data
-    train_data = pd.read_csv(path_database, sep=",", header=0, decimal=",", dtype=str)
+    # Adicione a nova coluna ao DataFrame
+    test_data = pd.concat([test_data, efficiency], axis=1)
     
+    predictor_class = TabularPredictor.load("AutogluonModels/ag-class_training", require_py_version_match=False)
+    # predictor_class.leaderboard(test_data, silent=True)
+    # Make predictions
+    predictions_class = predictor_class.predict(test_data)
+    
+    complexity_class = pd.Series([predictions_class[0]], name='complexity_class')
 
-    train_data = train_data.drop(['complexity'], axis=1)
+    # Adicione a nova coluna ao DataFrame
+    test_data = pd.concat([test_data, complexity_class], axis=1)
+    csv_file = str(test_data['filename'])
+    csv_file = csv_file.replace('.java', '.csv')
+    test_data.to_csv(csv_file, index=False)
+
+    return test_data
+
     
-    # which allows AutoGluon to automatically construct powerful model ensembles based on stacking/bagging, 
-    # and will greatly improve the resulting predictions if granted sufficient training time. 
-    # The default value of presets is 'medium_quality', which produces less accurate models but 
-    # facilitates faster prototyping. With presets, you can flexibly prioritize predictive 
-    # accuracy vs. training/inference speed. For example, if you care less about predictive performance and 
-    # want to quickly deploy a basic model, consider using: presets=['good_quality', 'optimize_for_deployment']
-    # Define the task (classification) and label column
-    
-    # Definir as colunas alvo (targets)
-    target_columns = ['efficiency', 'complexity_class']
-    predictor = TabularPredictor(label=target_columns, path = 'first_training').fit(train_data, presets=['good_quality', 'optimize_for_deployment'])
-    results = predictor.fit_summary(show_plot=True)
-    print(results)
-    return predictor
 
 
