818d4185b31268c16a3078889a5245a73bf43c7a
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-09-05 17:50:07 -0300

    autogluon improved

--- data_classificators/autogluon_classificator.py
+++ data_classificators/autogluon_classificator.py
@@ -3,38 +3,34 @@ import os
 import pandas as pd
 from autogluon.tabular import TabularPredictor
 
-import AutogluonModels
 from databases import *
 
 
 def autogluon_classifier(code_csv):
+    verbosity=4 # More training log. TESTAR NO TREINAMENTO, AQUI NÃO É RELEVANTE
     # os.environ['CUDA_VISIBLE_DEVICES'] = ''  # elimina uso da gpu no test
+    # eval_metrics = ['accuracy', 'precision', 'f1', 'recall'] TESTAR NO TREINAMENTO
     test_data = code_csv
     # 'f1' (for binary classification), 'roc_auc' (for binary classification),
     predictor_efficiency = TabularPredictor.load("AutogluonModels/ag-efficiency_training", require_py_version_match=False)
-    # predictor_efficiency.leaderboard(test_data, silent=True)
-    predictor_efficiency.plot_ensemble_model()# mostra pesos de cada modelo no emsemble
+    predictor_efficiency.plot_ensemble_model(filename = "efficiency_ensemble_model.png")# mostra pesos de cada modelo no emsemble
     # Make predictions
     predictions_efficiency = predictor_efficiency.predict(test_data)
-    results_efficiency = predictor_efficiency.fit_summary(show_plot=True)
+    # results_efficiency = predictor_efficiency.fit_summary(show_plot=True)
     
-    efficiency = pd.Series([predictions_efficiency[0]], name='efficiency')
+    test_data['efficiency'] = predictions_efficiency[0]
+    predictor_efficiency.leaderboard(test_data, silent=False) #extra_info = true
 
-    # Adicione a nova coluna ao DataFrame
-    test_data = pd.concat([test_data, efficiency], axis=1)
     
     predictor_class = TabularPredictor.load("AutogluonModels/ag-class_training", require_py_version_match=False)
-    # predictor_class.leaderboard(test_data, silent=True)
-    predictor_class.plot_ensemble_model()# mostra pesos de cada modelo no emsemble
     # Make predictions
     predictions_class = predictor_class.predict(test_data)
-    results_class = predictor_class.fit_summary(show_plot=True)
-
+    # results_class = predictor_class.fit_summary(show_plot=True)
+    predictor_class.plot_ensemble_model(filename = "complexity_classensemble_model.png")# mostra pesos de cada modelo no emsemble
     
-    complexity_class = pd.Series([predictions_class[0]], name='complexity_class')
+    test_data['complexity_class'] = predictions_class[0]
+    predictor_class.leaderboard(test_data, silent=False) #extra_info = true
 
-    # Adicione a nova coluna ao DataFrame
-    test_data = pd.concat([test_data, complexity_class], axis=1)
     csv_file = str(test_data['filename'])
     csv_file = csv_file.replace('.java', '.csv')
     test_data.to_csv(csv_file, index=False)
