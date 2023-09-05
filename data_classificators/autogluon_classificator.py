9a2ee4e011f87eaa71de954c2f207e72a1681814
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-09-05 18:59:25 -0300

    autogluon updated

--- data_classificators/autogluon_classificator.py
+++ data_classificators/autogluon_classificator.py
@@ -7,29 +7,36 @@ from databases import *
 
 
 def autogluon_classifier(code_csv):
+    custom_metrics = ['accuracy', 'precision', 'f1', 'recall']
     verbosity=4 # More training log. TESTAR NO TREINAMENTO, AQUI NÃO É RELEVANTE
     # os.environ['CUDA_VISIBLE_DEVICES'] = ''  # elimina uso da gpu no test
     # eval_metrics = ['accuracy', 'precision', 'f1', 'recall'] TESTAR NO TREINAMENTO
     test_data = code_csv
-    # 'f1' (for binary classification), 'roc_auc' (for binary classification),
+    # 'f1' (for binary classification), 'roc_auc' (for binary classification) TESTAR NO TREINAMENTO DA EFFICIENCY
     predictor_efficiency = TabularPredictor.load("AutogluonModels/ag-efficiency_training", require_py_version_match=False)
     predictor_efficiency.plot_ensemble_model(filename = "efficiency_ensemble_model.png")# mostra pesos de cada modelo no emsemble
-    # Make predictions
     predictions_efficiency = predictor_efficiency.predict(test_data)
-    # results_efficiency = predictor_efficiency.fit_summary(show_plot=True)
+    # results_efficiency = predictor_efficiency.fit_summary(show_plot=True) #descomentar 
     
     test_data['efficiency'] = predictions_efficiency[0]
+    # features_efficiency = predictor_efficiency.feature_importance(test_data) #aparentemente não muda muito
     predictor_efficiency.leaderboard(test_data, silent=False) #extra_info = true
-
+    
+    # predictor_efficiency.evaluate(test_data, extra_metrics=custom_metrics)
+    
+    
     
     predictor_class = TabularPredictor.load("AutogluonModels/ag-class_training", require_py_version_match=False)
-    # Make predictions
     predictions_class = predictor_class.predict(test_data)
-    # results_class = predictor_class.fit_summary(show_plot=True)
+    # results_class = predictor_class.fit_summary(show_plot=True) # descomentar
     predictor_class.plot_ensemble_model(filename = "complexity_classensemble_model.png")# mostra pesos de cada modelo no emsemble
     
     test_data['complexity_class'] = predictions_class[0]
+    # features_class = predictor_class.feature_importance(test_data) # aparentemente não muda muito
     predictor_class.leaderboard(test_data, silent=False) #extra_info = true
+    
+    # predictor_class.evaluate(test_data, extra_metrics=custom_metrics)
+    
 
     csv_file = str(test_data['filename'])
     csv_file = csv_file.replace('.java', '.csv')
