c0fb980df48c541b00c33b0194ded5940cc4227c
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-09-05 14:10:47 -0300

    autogluon improved

--- data_classificators/autogluon_classificator.py
+++ data_classificators/autogluon_classificator.py
@@ -8,12 +8,15 @@ from databases import *
 
 
 def autogluon_classifier(code_csv):
-        
+    # os.environ['CUDA_VISIBLE_DEVICES'] = ''  # elimina uso da gpu no test
     test_data = code_csv
+    # 'f1' (for binary classification), 'roc_auc' (for binary classification),
     predictor_efficiency = TabularPredictor.load("AutogluonModels/ag-efficiency_training", require_py_version_match=False)
     # predictor_efficiency.leaderboard(test_data, silent=True)
+    predictor_efficiency.plot_ensemble_model()# mostra pesos de cada modelo no emsemble
     # Make predictions
     predictions_efficiency = predictor_efficiency.predict(test_data)
+    results_efficiency = predictor_efficiency.fit_summary(show_plot=True)
     
     efficiency = pd.Series([predictions_efficiency[0]], name='efficiency')
 
@@ -22,8 +25,11 @@ def autogluon_classifier(code_csv):
     
     predictor_class = TabularPredictor.load("AutogluonModels/ag-class_training", require_py_version_match=False)
     # predictor_class.leaderboard(test_data, silent=True)
+    predictor_class.plot_ensemble_model()# mostra pesos de cada modelo no emsemble
     # Make predictions
     predictions_class = predictor_class.predict(test_data)
+    results_class = predictor_class.fit_summary(show_plot=True)
+
     
     complexity_class = pd.Series([predictions_class[0]], name='complexity_class')
 
@@ -45,6 +51,16 @@ def autogluon_classifier(code_csv):
 
     # Salva o DataFrame em um arquivo CSV dentro da pasta
     test_data.to_csv(csv_file_path, index=False)
+    
+    # import sklearn.metrics
+    # sklearn.metrics.accuracy_score(y_true, y_pred)
+    # from autogluon.core.metrics import make_scorer
+    # ag_accuracy_scorer = make_scorer(name='accuracy',
+    #                              score_func=sklearn.metrics.accuracy_score,
+    #                              optimum=1,
+    #                              greater_is_better=True)
+    # ag_accuracy_scorer(y_true, y_pred)
+    # predictor.leaderboard(test_data, extra_metrics=[ag_roc_auc_scorer, ag_accuracy_scorer], silent=True)
 
     return test_data
 
